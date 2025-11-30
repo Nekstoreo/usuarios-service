7fa75ed7c44dd60d2eeb86d03d74de38fdd55b9c
Nestor Gutierrez <nestorg456k@outlook.com>
2025-11-30 17:18:11 -0500

    feat: add domain models for user and role

---
+++ src/main/java/com/pragma/usuarios/domain/model/User.java
@@ -0,0 +1,103 @@
+package com.pragma.usuarios.domain.model;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String identityDocument;
+    private String phone;
+    private LocalDate birthDate;
+    private String email;
+    private String password;
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String identityDocument,
+                String phone, LocalDate birthDate, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.identityDocument = identityDocument;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdentityDocument() {
+        return identityDocument;
+    }
+
+    public void setIdentityDocument(String identityDocument) {
+        this.identityDocument = identityDocument;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+}
